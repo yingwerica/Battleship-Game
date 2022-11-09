f00f408f37b76094ddf5626a522d0f0f87682807
yingwerica <yingw.pro@gmail.com>
2022-11-08 20:44:13 -0700

    first time complete the game logic

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+//init variables
+//let continuePlay = prompt('Ready to attack?')
+
 // create battleship class for my battleships
 class Battleship {
     constructor(name, hull, firepower, accuracy) {
@@ -5,15 +8,7 @@ class Battleship {
         this.hull = hull;
         this.firepower = firepower;
         this.accuracy = accuracy;
-    }
-    
-    retreat(name, decision){
-        decision === "Y" ? console.log(`Game over! ${name} lost your base and Aliens win`)
-        : console.log(`Load up! ${name} is ready for another attack.`)
-    }
-    destroyed(name){
-        this.console.log(`Oh nooooo! ${name} is down! Who is up next?`)
-    }
+    }  
 }
 
 //create an object of my battleship
@@ -30,81 +25,103 @@ class Alienship {
     }
 
 }
+// const alienShip1 = new Alienship('one')
+// console.log(alienShip1);
 
 //create alien ship array
-let alienShip;
-const teamAlien = [];
+let teamAlien = [];
 
 function array(n){
   for (let i = 0; i < n; i++) {
-    alienShip = new Alienship(i);
-    teamAlien.push(alienShip); 
+    teamAlien[i] = new Alienship('Alienship' + i);    
   };
 };
 array(6);
-// console.log(teamAlien);
+console.log(teamAlien);
 
+// /////////////another way to create alienShip array
+// let alienShipTeam=[];
+// for(let i=0;i<6;i++)
+// {
+// let enemy={};
+// enemy.name = "alienShip"+i;
+// enemy.hull = Math.floor(Math.random() * 4) + 3;
+// enemy.firepower = Math.floor(Math.random() * 3) + 2;
+// enemy.accuracy = (Math.floor(Math.random() * 3) + 6) / 10;
+// alienShipTeam.push(enemy);
+// enemy = {};
+// }
+// console.log(alienShipTeam)
+///////////////////////////////////////////////////////////
 
-
-
-
-
-let playerAnswer = 'yes'
 //create attackAlien function for battleship
-function attackAlien(myShip, alienShip) {
-    
-    if (myShip.hull > 0) {
-        //launch attack
-        if (Math.floor(Math.random()) <= myShip.accuracy) {   // full hit
-            console.log('You hit!');
-            alienShip.hull-= myShip.firepower;
-            
-            
-            if (alienShip.hull > 0) {
-                attackBack(alienShip, myShip); // alien still alive, so attack back
-            } else {                // one alien destroyed                              
-                //delete destroyed ship from teamAlien
-                if (teamAlien.indexOf(alienShip) > -1) {  // only splice array when item is found
-                    teamAlien.splice(0, 1);
-                    console.log("Great! You destroy an alien ship! Ready to next round?")
-                    if (playerAnswer === 'yes') { //choose proceed to another attack
-                    attackAlien(myShip, teamAlien[0]);
-                    } else {
-                        console.log('Game over!') //choose retreat game over
-                    }
-                } else {             // alien ships all destroyed
-                    console.log("Hooray! USSStrongArm win the game!") 
-                }
-                               
-            }
-        }else {     // attack missed, so alien attack back
-            console.log('oh no, you missed. ');
-            attackBack(alienShip, myShip);
-    
-        }
-    } else {    //hull <= 0, destroyed
-        console.log('Game over, you are destroyed')
+function attackAlien(alienShip) {
+    if (Math.floor(Math.random()) <= myBattleship.accuracy) {   // full hit
+        document.getElementById("alertMsg").innerHTML = 'Geart!You hit the target!'; 
+        alienShip.hull-= myBattleship.firepower;
+        document.getElementById("alertMsg").innerHTML = `${alienShip.name} hull: ${alienShip.hull}`;
+        
+    } else {                                                     // you missed
+        document.getElementById("alertMsg").innerHTML = 'Bad luck, you missed....';
+        document.getElementById("alertMsg").innerHTML = `${alienShip.name} hull: ${alienShip.hull}`
+        
+    }   
+};
+
+//create alien attack back function
+function alienAttack(alienShip) {
+    if (Math.floor(Math.random()) <= alienShip.accuracy) {   // full hit
+        document.getElementById("alertMsg").innerHTML ='oh no!!You got hit by an alien ship!';
+        myBattleship.hull-=alienShip.firepower;
+        document.getElementById("alertMsg").innerHTML = `${myBattleship.name} hull: ${myBattleship.hull}`
+        
+    } else {
+        document.getElementById("alertMsg").innerHTML ='Yeah!The alien ship missed!';
+        document.getElementById("alertMsg").innerHTML = `${myBattleship.name} hull: ${myBattleship.hull}` 
+                                               // alien missed
     }
+};
+
+//delete the destroyed alienship
+// function alienDestroyed(alienShip) {
+//     if (alienShip.hull <= 0) {
+//         teamAlien.shift();   ///or use .splice(0,1)
+//     }
+// }
+
+//create gameOver function
+function gameOver() {
+    document.getElementById("promptChoice").innerHTML = '';
+    document.getElementById("retreat").remove();
+    document.getElementById("attack").remove();
+    document.getElementById("alertMsg").innerHTML = 'Game over. Alien invasion! Refresh the page to play again.'
 }
 
+//create button click event
+document.getElementById("retreat").onclick = function(){gameOver()};
+document.getElementById("attack").onclick = function(){gameRound(teamAlien[0])};
 
+let endThisRound = false;
 
-//create attackBack function of alien ship
-function attackBack(alienShip, myShip) {
-    if (Math.floor(Math.random()) <= alienShip.accuracy) { //alien hit
-        myShip.hull-= alienShip.firepower;
-        // check if ship destroyed
-        if (myShip.hull > 0) {    //myship still alive, go attack alien
-            attackAlien(myShip, alienShip);
-            console.log('They did not destroy you, keep on your attack!')
-        } else {  //alien destroyed you
-            console.log('oooops, you are dead... game over')
+function gameRound(alienShip) {
+    document.getElementById("promptChoice").innerHTML = '';
+    document.getElementById("attack").disabled = true;
+    document.getElementById("retreat").disabled = true;
+    do {attackAlien(alienShip);
+        if (alienShip.hull > 0){
+            alienAttack(alienShip);
+        } else if(alienShip <= 0) {
+            teamAlien.shift();
+            document.getElementById("attack").disabled = false;
+            document.getElementById("retreat").disabled = false;
+            document.getElementById("promptChoice").innerHTML = 'Hooray!An Alien ship is destroyed! \n Press "attack" button to knock down them all, or press "retreat" button to end this battle.'    
+            endThisRound = true;
         }
-
-    } else {    //alien missed
-        attackAlien(myShip, alienShip);
-    };
+    } while (myBattleship.hull > 0 && endThisRound == false);
+    
+    if (myBattleship.hull <= 0){
+        document.getElementById("alertMsg").innerHTML = 'You are down! You are down!'
+        gameOver();
+    }
+    
 }
-
-
-console.log(attackAlien(myBattleship, teamAlien[0]));
